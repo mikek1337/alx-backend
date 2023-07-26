60e9f5b8616f21023ea16f702197edcc35167081
mikek1337 <mikekegnlyew@gmail.com>
2023-07-26 19:08:00 +0300

    inital commit for caching

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""Basic caching module"""
+
+from base_caching import BaseCaching
+
+
+class BasicCache(BaseCaching):
+    def put(self, key, item):
+        """Adds an item in the cache"""
+        if key is not None and item is not None:
+            self.cache_data[key] = item
+    
+    def get(self, key):
+        """Gets an item from cache"""
+        if key is not None and key in self.cache_data:
+            return self.cache_data[key]
+        return None
