1576f760b76c41a1a97799e6d5ec5ee25e41f265
mikek1337 <mikekegnlyew@gmail.com>
2023-08-28 21:11:39 +0300

    refactor code

--- 0x03-queuing_system_in_js/1-redis_op.js
+++ 0x03-queuing_system_in_js/1-redis_op.js
@@ -4,24 +4,26 @@ const client = createClient();
 
 client.on("connect", () => {
     console.log("Redis client connected to the server");
-    displaySchoolValue('Holberton');
-    setNewSchool('HolbertonSanFrancisco', '100');
-    displaySchoolValue('HolbertonSanFrancisco');
+
 }).on("error", (err) => {
     console.log(`Redis client not connected to the server: ${err}`);
 })
 
 
-async function setNewSchool(schoolName, value) {
+function setNewSchool(schoolName, value) {
     client.set(schoolName, value, (err, reply) => {
         console.log(`Reply: ${reply}`);
     })
 
 }
 
-async function displaySchoolValue(schoolName) {
+function displaySchoolValue(schoolName) {
     client.get(schoolName, (err, reply) => {
         console.log(reply)
     })
 }
+
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
 
