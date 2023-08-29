55672c5c84302ddfa4a5cbaf4aae221590347aec
Mikek1337 <mikekegnlyew@gmail.com>
2023-08-29 11:13:36 +0300

    changed displaySchoolValue to async

---
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -0,0 +1,30 @@
+import { createClient } from "redis";
+import { promisify } from "util";
+const client = createClient();
+
+client
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  })
+  .on("error", (err) => {
+    console.log(`Redis client not connected to the server: ${err}`);
+  });
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, (err, reply) => {
+    console.log(`Reply: ${reply}`);
+  });
+}
+
+async function displaySchoolValue(schoolName) {
+  const asyncGet = promisify(client.get).bind(client)(schoolName);
+  console.log(await asyncGet);
+}
+
+async function main() {
+  await displaySchoolValue("Holberton");
+  setNewSchool("HolbertonSanFrancisco", "100");
+  displaySchoolValue("HolbertonSanFrancisco");
+}
+
+main();
